9cc7aabdfa99edb1cc8044988b27fc5b2ae00d16
Lichfolky <lichfolky@gmail.com>
2023-07-21 21:50:28 +0200

    score

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 const canvas = document.getElementById("scene");
+const scoreEl = document.querySelector(".score");
 const ctx = canvas?.getContext("2d");
 
 let x = 0;
 let y = 0;
 let loadinglight = true;
 let loadingdark = true;
-let imgdark = new Image();
-let imglight = new Image();
+const imgdark = new Image();
+const imglight = new Image();
 let mouseout = true;
+let score = 0;
+let pointsX = [22, 600, 800, 400, 615];
+let pointsY = [420, 510, 370, 130, 145];
+const tollerance = 20;
+
 
 if (ctx) {
+    scoreEl.textContent = score;
     canvas.addEventListener("mousemove", (event) => {
         const rect = canvas.getBoundingClientRect();
         x = event.clientX - rect.left;
         y = event.clientY - rect.top;
+
+        for (let i = 0; i < pointsX.length; i++) {
+            if (pointsX[i] <= x + tollerance && pointsX[i] >= x - tollerance) {
+                if (pointsY[i] <= y + tollerance && pointsY[i] >= y - tollerance) {
+                    score++;
+                    pointsX.splice(i, 1);
+                    pointsY.splice(i, 1);
+                }
+            }
+        }
+        scoreEl.textContent = score + " " + x + " " + y;
+
         mouseout = false;
     });
 
